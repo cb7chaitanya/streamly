a51bb9cd77cf19ca999b754a4f6846522f88cd5c
Chaitanya Bajpai <cb7chaitanya@gmail.com>
2024-10-08 05:01:33 +0530

    [FEAT] Creating express server

--- apps/user-be/src/index.ts
+++ apps/user-be/src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello from TypeScript project!');
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+app.listen(3000, () => {
+    console.log('Listening on PORT 3000')
+})
