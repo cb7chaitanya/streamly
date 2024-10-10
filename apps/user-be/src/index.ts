55b7faf4528e8ac78ad4ffe9c3dcf094e39db9ef
Chaitanya Bajpai <cb7chaitanya@gmail.com>
2024-10-10 20:06:56 +0530

    [FEAT] Accessing primary router in index file

--- apps/user-be/src/index.ts
+++ apps/user-be/src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import cors from 'cors'
+import primaryRouter from './routes'
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
+app.use('/api/v1', primaryRouter)
 
 app.listen(3000, () => {
     console.log('Listening on PORT 3000')
