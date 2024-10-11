ff818d5ce7e878729779e1e4c05524342c564fa8
Chaitanya Bajpai <cb7chaitanya@gmail.com>
2024-10-11 18:04:58 +0530

    [FEAT] Integrating custom validator and db packages in the backends

--- apps/rtc-be/src/ffmpeg.ts
+++ apps/rtc-be/src/ffmpeg.ts
@@ -2,10 +2,15 @@ import { spawn, ChildProcessWithoutNullStreams } from "child_process"
 import ffmpegPath from "ffmpeg-static"
 import { Readable } from "stream"
 
-const handleStream = (streamData: Readable, rtmpUrl: string): void => {
-    const ffmpeg: ChildProcessWithoutNullStreams = spawn(ffmpegPath!, [
+const handleStream = (streamData: Readable, rtmpUrl: string): ChildProcessWithoutNullStreams => {
+    const ffmpeg: ChildProcessWithoutNullStreams = spawn(ffmpegPath.toString(), [
         '-i', 'pipe:0',
         '-f', 'flv',
+        '-re',
+        '-c:v', 'libx264',
+        '-c:a', 'aac',
+        '-preset', 'veryfast',
+        '-b:a', '128k',
         rtmpUrl
     ])
 
@@ -18,6 +23,8 @@ const handleStream = (streamData: Readable, rtmpUrl: string): void => {
     ffmpeg.on('close', (code) => {
         console.log(`FFmpeg exited with code ${code}`)
     })
+
+    return ffmpeg
 }
 
 export default handleStream
--- apps/rtc-be/src/index.ts
+++ apps/rtc-be/src/index.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 import WebSocket from 'ws'
 import { Request, Response } from 'express'
-import handleStream from './ffmpeg'
-import { Message } from './types'
+import handleStream from './ffmpeg.ts'
+import { Message } from './types.ts'
 import { Readable } from 'stream'
-
+import { ChildProcessWithoutNullStreams } from 'child_process'
+import { messageSchema } from '@repo/validator/client'
 
 const app = express()
 const port = 8080
@@ -15,18 +16,32 @@ app.use(express.json())
 
 wss.on('connection', (ws) => {
     console.log('Client connected')
+    let ffmpegProcess: ChildProcessWithoutNullStreams | null = null
 
     ws.on('message', (message: Message) => {
+        const { success } = messageSchema.safeParse(message)
+        if(!success) {
+            console.error('Invalid message:', message)
+            process.exit(1);
+        }
         const { rtmpUrl, streamData } = message
         console.log('Received stream data from client:', streamData)
         const readableStream = new Readable()
         readableStream.push(streamData)
         readableStream.push(null)
-        handleStream(streamData, rtmpUrl)
+        ffmpegProcess = handleStream(streamData, rtmpUrl)
     })
 
     ws.on('close', () => {
         console.log('Client disconnected')
+        if(ffmpegProcess) {
+            ffmpegProcess.kill()
+            console.log('FFmpeg process killed')
+        }
+    })
+
+    ws.on('error', (error) => {
+        console.error('WebSocket error:', error)
     })
 })
 
