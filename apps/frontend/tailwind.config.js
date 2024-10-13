7c087e46119ab872613884ca4aead75ca3ca00a2
Chaitanya Bajpai <cb7chaitanya@gmail.com>
2024-10-13 21:36:37 +0530

    [FEAT] Configuring Font

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+        fontFamily: {
+          'neue-helvetica': ['NeueHelevetica', 'Helvetica', 'Arial', 'sans-serif'],
+        },
+    },
   },
   plugins: [],
 }
