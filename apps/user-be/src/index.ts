3e2033dda8550464d5ddb6f43c26a56d48ce90fe
Chaitanya Bajpai <cb7chaitanya@gmail.com>
2024-10-13 17:13:13 +0530

    [FEAT] Configuring cookie parser

--- apps/user-be/src/index.ts
+++ apps/user-be/src/index.ts
@@ -1,10 +1,22 @@
 import express from 'express'
 import cors from 'cors'
 import primaryRouter from './routes/index.js'
+import cookiesParser from 'cookie-parser'
 
 const app = express()
+
 app.use(express.json())
-app.use(cors())
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
+
+app.use(cookiesParser())
+
+app.get('/', async(req, res) => {
+    res.send('Hello World!')
+})
 
 app.use('/api/v1', primaryRouter)
 
