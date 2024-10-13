9ff05b2cbc7105c78b8a98de94e8636d4000a2f7
Chaitanya Bajpai <cb7chaitanya@gmail.com>
2024-10-13 21:39:49 +0530

    [FEAT] Configured React Router

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -1,3 +1,6 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Pricing from './pages/Pricing.tsx'
+import Signup from './pages/Signup.tsx'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
@@ -5,6 +8,12 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
 )
