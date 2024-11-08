33be612172e1fe33de7cd52deac513209e7a9286
Chaitanya Bajpai <cb7chaitanya@gmail.com>
2024-11-08 22:50:28 +0530

    [FEAT] Layout components created

--- apps/frontend/src/components/Layout/Footer.tsx
+++ apps/frontend/src/components/Layout/Footer.tsx
@@ -1,6 +1,6 @@
-const Footer = () => {
+const Footer = ({className}: {className?: string}) => {
   return (
-    <footer className="py-6 text-center text-gray-500 text-sm">
+    <footer className={`py-6 text-center text-gray-500 text-sm ${className}`}>
       © 2023 Streamly. All rights reserved.
     </footer>
   );
--- apps/frontend/src/components/Layout/Navbar.tsx
+++ apps/frontend/src/components/Layout/Navbar.tsx
@@ -1,12 +1,14 @@
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <header className="py-6 px-8 flex justify-between items-center">
         <Link to="/"><h1 className="text-3xl font-extrabold tracking-tight bg-clip-text text-transparent bg-gradient-to-r from-gray-400 to-white">Streamly</h1></Link>
-        <nav>
-            <Link to="/pricing" className="text-gray-400 hover:text-white mx-3 transition duration-300">Pricing</Link>
-            <Link to="/signup" className="bg-gray-700 hover:bg-gray-600 text-white font-medium py-2 px-4 rounded-full transition duration-300">Sign Up</Link>
+        <nav className='flex justify-center items-center gap-4'>
+            <Link to="/pricing" className="text-gray-400 hover:text-white transition duration-300">Pricing</Link>
+            <Link to="/signup" className="text-gray-400 hover:text-white transition duration-300">Sign Up</Link>
+            <WalletMultiButton />
         </nav>
     </header>
   )
