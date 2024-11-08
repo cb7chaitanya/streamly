a8d7cd65f6f48e90257ed352769fe84c73fd3659
Chaitanya Bajpai <cb7chaitanya@gmail.com>
2024-11-08 22:48:54 +0530

    [FEAT] Setup Wallet Provider

---
+++ apps/frontend/src/components/Layout/Provider.tsx
@@ -0,0 +1,24 @@
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
+import '@solana/wallet-adapter-react-ui/styles.css';
+import { useMemo } from "react";
+
+const Provider = ({children}: {children: React.ReactNode}) => {
+    const network = WalletAdapterNetwork.Devnet;
+    const endpoint: string = useMemo(() => clusterApiUrl(network), [network])
+    return (
+        <div className='bg-white h-screen w-full'>
+            <ConnectionProvider endpoint={endpoint}>
+                <WalletProvider wallets={[]}>
+                    <WalletModalProvider>
+                        {children}
+                    </WalletModalProvider>
+                </WalletProvider>
+            </ConnectionProvider>
+        </div>
+    )
+}
+
+export default Provider
--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -2,18 +2,23 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Pricing from './pages/Pricing.tsx'
 import Signup from './pages/Signup.tsx'
 import React from 'react'
+import Login from './pages/Login.tsx'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import Provider from './components/Layout/Provider.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Provider>
     <Router>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
+    </Provider>
   </React.StrictMode>
 )
