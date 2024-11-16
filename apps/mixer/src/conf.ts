a680daf42007d5f0768b3333281b0493cbaaab94
Chaitanya Bajpai <cb7chaitanya@gmail.com>
2024-11-17 00:25:09 +0530

    [CHORE] Env configuration

--- apps/mixer/src/conf.ts
+++ apps/mixer/src/conf.ts
@@ -3,3 +3,6 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const JWT_SECRET = process.env.JWT_SECRET
+export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
+export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
+export const FRONTEND_BASE_URI = process.env.FRONTEND_BASE_URI
