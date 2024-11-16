b97ab716d6cc6fb2bf90fb3b24288fd70566c424
Chaitanya Bajpai <cb7chaitanya@gmail.com>
2024-11-17 00:20:38 +0530

    [INIT] Initialised the sfu server

--- apps/sfu-server/src/index.ts
+++ apps/sfu-server/src/index.ts
@@ -1,4 +1,13 @@
 import express from 'express'
-import mediasoup from 'mediasoup'
 import { WebSocketServer } from 'ws'
 import { createServer } from 'http'
+
+const app = express()
+const server = createServer(app)
+const wss = new WebSocketServer({ server })
+
+app.use(express.json())
+
+server.listen(8081, () => {
+    console.log('Server listening on port 8081')
+})
