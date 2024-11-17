cccff155d0e3fdda4f410f3707e207b0b9b41e16
Chaitanya Bajpai <cb7chaitanya@gmail.com>
2024-11-18 03:27:46 +0530

    [FEAT] Setting up dependencies

--- apps/sfu-server/src/index.ts
+++ apps/sfu-server/src/index.ts
@@ -1,12 +1,36 @@
 import express from 'express'
 import { WebSocketServer } from 'ws'
 import { createServer } from 'http'
+import { RoomManager } from './roomManager.js'
+import url from 'url'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import { extractUserId } from './auth.js'
+import { createWorker } from './lib/worker.js'
+import { workerSettings } from './rtc-config.js'
+import { Peer } from './peer.js'
 
 const app = express()
 const server = createServer(app)
 const wss = new WebSocketServer({ server })
+const roomManager = new RoomManager()
 
-app.use(express.json())
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
+app.use(cookieParser())
+
+wss.on('connection', async function connection(ws, req) {
+    
+    const userId = extractUserId(req.headers.cookie) 
+    const worker = await createWorker(workerSettings)
+    roomManager.addPeer(new Peer(ws, userId), worker)
+
+    ws.on('close', () => {
+        roomManager.removePeer(ws)
+    })
+})
 
 server.listen(8081, () => {
     console.log('Server listening on port 8081')
