241e04349878a3520d3bfee319418458ee58f99b
Chaitanya Bajpai <cb7chaitanya@gmail.com>
2024-11-19 05:05:30 +0530

    [CHORE] env config

---
+++ apps/frontend/src/config/config.ts
@@ -0,0 +1,3 @@
+export const USER_BACKEND_URL = import.meta.env.VITE_USER_BACKEND_URL
+export const RTC_BACKEND_URL = import.meta.env.VITE_RTC_BACKEND_URL
+export const MIXER_BACKEND_URL = import.meta.env.VITE_MIXER_BACKEND_URL
